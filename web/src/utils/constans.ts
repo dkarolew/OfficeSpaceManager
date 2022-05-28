341dbe25936cc97a41a1b0d619403e1654574248
Dawid Karolewski <dawid.karolewski97@gmail.com>
2022-05-28 20:29:15 +0200

    Update api gcp url

--- web/src/utils/constans.ts
+++ web/src/utils/constans.ts
@@ -1,2 +1,2 @@
-export const API_BASE_URL = "http://34.68.195.49:8080/api/v1";
+export const API_BASE_URL = "http://35.239.109.119:8080/api/v1";
 // export const API_BASE_URL = "http://localhost:8080/api/v1";
