cd2b1765ea594e73a589cb0599763341dc704ac5
Dawid Karolewski <dawid.karolewski97@gmail.com>
2022-05-28 22:19:47 +0200

    Update api url

--- web/src/utils/constans.ts
+++ web/src/utils/constans.ts
@@ -1,2 +1,2 @@
-export const API_BASE_URL = "http://34.66.82.197:8080/api/v1";
+export const API_BASE_URL = "http://34.68.195.49:8080/api/v1";
 // export const API_BASE_URL = "http://localhost:8080/api/v1";
